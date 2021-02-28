84adb33ea306fed5512563514fb5334379669a4b
fahimkk <fahimasharafkk@gmail.com>
2021-02-28 13:27:14 +0530

    Complete save button functionality.

--- main.go
+++ main.go
@@ -101,15 +101,21 @@ func index(response http.ResponseWriter, request *http.Request) {
 	} else {
 		// else it is POST method
 		request.ParseForm()
-		// If it insert
 		title = request.Form.Get("title")
 		description = request.Form.Get("description")
 		deleteID := request.Form.Get("deleteID")
 		completedID := request.Form.Get("completedID")
+		editID := request.Form.Get("editID")
+		uId := request.Form.Get("uId")
+		uTitle := request.Form.Get("uTitle")
+		uDescription := request.Form.Get("uDescription")
+
 		fmt.Println(completedID)
 		fmt.Println("d.ID: ",deleteID)
 		// r.Form gives a map of values, get gives single value
 		// add title to db if not empty
+
+		// Insert to database
 		if len(title) !=0 {
 			// INSERT/
 			stmt, err := db.Prepare("INSERT list SET title=?, description=?, status=?")
@@ -128,7 +134,7 @@ func index(response http.ResponseWriter, request *http.Request) {
 	
 		}
 		// Delete from Database
-			if deleteID != "" {
+		if deleteID != "" {
 			id, err = strconv.ParseInt(deleteID,10,64)
 			checkErr(err)
 			stmt, err := db.Prepare("DELETE from list WHERE id=?") 
@@ -150,14 +156,14 @@ func index(response http.ResponseWriter, request *http.Request) {
 						toDo.Completed = append(toDo.Completed[:index], toDo.Completed[index+1:]...)
 						itemfound = false 
 						break
+					}
 				}
-			}
 	
+			}
 		}
-	}
 		// if status changed to completed, then
 		// move task form incompleteTasks to completedTasks 
-			if completedID != "" {
+		if completedID != "" {
 			id, err = strconv.ParseInt(completedID,10,64)
 			checkErr(err)
 			stmt, err := db.Prepare("UPDATE list SET status=? WHERE id=?")
@@ -175,6 +181,40 @@ func index(response http.ResponseWriter, request *http.Request) {
 			}
 			// Add to completedTasks
 			toDo.Completed = append(toDo.Completed, task{ID:id, Title: title, Status: status})
+		}
+		if editID != ""{
+			id, err = strconv.ParseInt(editID,10,64)
+			checkErr(err)
+			// Cancel button is only for incomplete tasks. search for id and return a map of title and description.
+			for _, item := range toDo.Incomplete{
+				if item.ID == id {
+					title = item.Title
+					description = item.Description
+					break
+				}
+			}
+			m := map[string]string{"title":title, "description":description}
+			b, _ := json.Marshal(m)
+			response.Write(b)
+		}
+
+		// Update data
+		if uId !="" {
+			id, err = strconv.ParseInt(uId,10,64)
+			checkErr(err)
+			stmt, err := db.Prepare("UPDATE list SET title=?, description=? WHERE id=?")
+			checkErr(err) 
+			_, err = stmt.Exec(uTitle, uDescription, id)
+			checkErr(err)
+			// Update from the incompleteTasks
+			// when an existing item is completed then only completedID will post, title will be empty 
+			for _, item := range toDo.Incomplete{
+				if item.ID == id {
+					item.Title = uTitle
+					item.Description = uDescription
+					break
+				}
+			}
 		}
 	}
 }
@@ -219,6 +259,6 @@ func main(){
 	http.HandleFunc("/", index)
 	// http.HandleFunc("/getCompletedTasks", getCompletedTasks)
 
-	err := http.ListenAndServe(":9000", nil)
+	err := http.ListenAndServe(":9090", nil)
 	checkErr(err)
 }
